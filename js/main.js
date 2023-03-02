657066c340be578160721b18f39ff3a44a2c13fe
Sandoval Melo <juniormelo777@gmail.com>
2023-03-02 18:29:04 -0300

    add toggle menu scripts

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log("Hello");
+const toggleIcon = document.getElementById("toggle-icon");
+const nav = document.getElementById("nav");
+const menuIcon = document.getElementById("menu-icon");
+
+toggleIcon.addEventListener("click", () => {
+	nav.classList.toggle("active");
+
+	if (nav.classList.contains("active")) {
+		menuIcon.setAttribute("src", "./images/icon-menu-close.svg");
+		document.body.style.overflow = "hidden";
+	}
+
+	if (!nav.classList.contains("active")) {
+		menuIcon.setAttribute("src", "./images/icon-menu.svg");
+		document.body.style.overflow = "auto";
+	}
+});
